b83dba7377ce885182cb4247fa3d526bd0f6830e
mbasaran77 <mbasaran77@gmail.com>
2023-10-01 07:40:04 +0300

    cikarma fonksiyonu eklendi

--- test_1.py
+++ test_1.py
@@ -1,11 +1,19 @@
-def toplam(a :int, b:int):
+"""Module providing a function printing python version."""
+
+def toplam(sayi_1: int, sayi_2: int):
     """
     a:int b : int 
     return sum
     """
-    return a + b
+    return sayi_1 + sayi_2
 
+def cikarma(sayi_1: int, sayi_2: int):
+    """
+    a:int b : int
+    return substraction
+    """
+    return sayi_1 - sayi_2
 
 if __name__ =="__main__":
-    print(toplam(5,4))
-    
+    print(toplam(5, 4))
+    print(cikarma(1, 9))
